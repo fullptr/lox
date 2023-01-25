520bb6612666bb86ee098eb6af0c4205ece60ada
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-01-25 01:59:53 +0000

    Add scanner

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -74,15 +75,14 @@ static InterpretResult run()
             }
         }   
     }
-    
+
 #undef BINARY_OP
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
 
-InterpretResult interpret(Chunk* chunk)
+InterpretResult interpret(const char* source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
