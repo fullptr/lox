1ee5d443d5164b5f11b65e7a2f4eb0a4fbdd001d
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2024-07-08 01:24:44 +0100

    Implement chapter 23

--- src/common.h
+++ src/common.h
@@ -3,7 +3,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-//#define DEBUG_PRINT_CODE
-//#define DEBUG_TRACE_EXECUTION
+#if 0
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+#endif
 
 #define UINT8_COUNT (UINT8_MAX + 1)
